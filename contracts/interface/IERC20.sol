a32d9707cedf4111f673e816b218fb7b806137d6
Muema Kamba <muemakamba@gmail.com>
2022-05-31 15:21:57 +0300

    IERC20 update

--- contracts/interface/IERC20.sol
+++ contracts/interface/IERC20.sol
@@ -1,13 +1,33 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.4;
+interface IERC20Joe {
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-interface IERC20 {
-  function totalSupply() external view returns (uint);
-  function balanceOf(address account) external view returns (uint);
-  function transfer(address recipient, uint amount) external returns (bool);
-  function allowance(address owner, address spender) external view returns (uint);
-  function approve(address spender, uint amount) external returns (bool);
-  function transferFrom(address sender, address recipient, uint amount) external returns (bool);
-  event Transfer(address indexed from, address indexed to, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function decimals() external view returns (uint8);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address owner) external view returns (uint256);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 value) external returns (bool);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
 }
